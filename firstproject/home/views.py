f2bf0d500a2ba37856c0c119ad75ffd117f2baa4
Nikhil Ambure <nikhilambure25@gmail.com>
2024-10-01 19:25:09 +0530

    Dynamic-Routing

--- firstproject/home/views.py
+++ firstproject/home/views.py
@@ -4,7 +4,10 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    return HttpResponse('Hello Client !!')
+    return render(request, 'index.html')
 
 def contact(request):
-    return HttpResponse('Contact me.')
+    return render(request, 'contact.html')
+
+def dynamic_route(request, number):
+    return HttpResponse(f"Dynamic route by number {number}")

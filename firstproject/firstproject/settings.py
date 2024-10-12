7cc2a716af104fbe9d402a4dac00b6b2f50950a6
Nikhil Ambure <nikhilambure25@gmail.com>
2024-10-12 14:38:39 +0530

    Relationships

--- firstproject/firstproject/settings.py
+++ firstproject/firstproject/settings.py
@@ -74,10 +74,22 @@ WSGI_APPLICATION = 'firstproject.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+# POSTGRES
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'mydb',
+        'USER': 'postgres',
+        'PASSWORD': '1702',
+        'HOST': 'localhost',
+        'PORT': ''
     }
 }
 

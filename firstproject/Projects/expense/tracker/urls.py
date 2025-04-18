404c876864fa184e69b6c5280cc8d9ae56c0e0e6
Nikhil Ambure <nikhilambure25@gmail.com>
2025-04-18 19:29:48 +0530

    Crud-Revision

--- firstproject/Projects/expense/tracker/urls.py
+++ firstproject/Projects/expense/tracker/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from tracker.views import index
+from tracker.views import index, delTransaction
 
 urlpatterns = [
-    path('', index),
+    path('', index, name="index"),
+    path('del-Transaction/<uuid>/', delTransaction, name="delTransaction"),
 ]
--- firstproject/Projects/expense/tracker/views.py
+++ firstproject/Projects/expense/tracker/views.py
@@ -1,6 +1,37 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from tracker.models import Transaction
+from django.db.models import Sum
 
 # Create your views here.
 
 def index(request):
-    return render(request, "index.html")
+    if request.method == "POST":
+        description = request.POST.get('description')
+        amount = request.POST.get('amount')
+
+        if description is None:
+            messages.info(request, "Description cannot be blank")
+            return redirect('/')
+
+        try:
+            amount = float(amount)
+        except Exception as e:
+            messages.info(request, "Should be a Number")
+            return redirect('/')
+        
+        Transaction.objects.create(amount=amount, description=description)
+
+        return redirect('/')
+    
+    context = {'transactions': Transaction.objects.all(),
+               'balance': Transaction.objects.all().aggregate(total_balance = Sum('amount'))['total_balance'] or 0,
+               'income' : Transaction.objects.filter(amount__gte = 0).aggregate(income = Sum('amount'))['income'] or 0,
+               'expense': Transaction.objects.filter(amount__lte = 0).aggregate(expense = Sum('amount'))['expense'] or 0,
+               }
+    return render(request, "index.html", context)
+
+
+def delTransaction(request, uuid):
+    Transaction.objects.get(uuid = uuid).delete()
+    return redirect('/')

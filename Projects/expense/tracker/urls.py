08142a5735fda6bec1697bcd78f32fbc27cb01c8
Nikhil Ambure <nikhilambure25@gmail.com>
2025-05-02 18:03:41 +0530

    Authenticate

--- Projects/expense/tracker/urls.py
+++ Projects/expense/tracker/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
-from tracker.views import index, delTransaction
+from tracker.views import index, delTransaction, registration, LoginPage, LogoutPage
 
 urlpatterns = [
     path('', index, name="index"),
+    path('registration/', registration, name='registration'),
+    path('login/', LoginPage, name="login"),
+    path('logout/', LogoutPage, name="logout"),
     path('del-Transaction/<uuid>/', delTransaction, name="delTransaction"),
+    
 ]
--- Projects/expense/tracker/views.py
+++ Projects/expense/tracker/views.py
@@ -1,11 +1,91 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from tracker.models import Transaction
-from django.db.models import Sum
+from django.db.models import Sum, Q
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
 
 # Create your views here.
 
+# User Authentication
+def registration(request):
+    if request.method == 'POST':
+        first_name  = request.POST.get('first_name')
+        last_name  = request.POST.get('last_name')
+        username  = request.POST.get('username')
+        email  = request.POST.get('email')
+        password  = request.POST.get('password')
+
+        
+        # If username or email already exists
+        user_obj = User.objects.filter(
+            Q(email = email) |  Q(username = username)
+            )
+
+        if user_obj.exists():
+            messages.error(request, "Username or Email Already Exist")
+            return redirect('/registration/')
+        
+
+        # You cannot pass the password here because it is encrypted
+        user = User.objects.create(
+            first_name = first_name,
+            last_name = last_name,
+            username = username,
+            email = email
+        )
+
+        # To pass the password
+        user.set_password(password)
+        user.save()
+        messages.error(request, 'Success : Account Created')
+        return redirect('/registration/')
+
+
+    return render(request, 'registration.html')
+
+
+# Login logic
+def LoginPage(request):
+    if request.method == 'POST':
+        username  = request.POST.get('username')
+        password  = request.POST.get('password')
+
+        user_obj = User.objects.filter(username = username)
+
+        # To check whether the user exists or not
+        if not user_obj.exists():
+            messages.error(request, 'Error: User does not exists')
+            return redirect('/login/')
+
+
+        # whether the username and password matches or not
+        # if does not matches 'authenticate' returns None
+        user_obj = authenticate(username = username, password = password)
+
+        if not user_obj:
+            messages.error(request, 'Error: Invalid Crendentials')
+            return redirect('/login/')
+
+        
+        login(request, user_obj)
+        return redirect('/')
+    
+    return render(request, 'login.html')
+
+
+# Logout Logic
+def LogoutPage(request):
+    logout(request)
+    messages.error(request, 'Success: Logged out successfully.')
+    return redirect('/login/')
+
+
 def index(request):
+
+    # To check whether user is authenticated
+    print(request.user)
+
     if request.method == "POST":
         description = request.POST.get('description')
         amount = request.POST.get('amount')

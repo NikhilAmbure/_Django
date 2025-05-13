7ff7427d597ce389bdcf083526bb1c00e1a53ca2
Nikhil Ambure <nikhilambure25@gmail.com>
2025-05-13 18:24:24 +0530

    Downloading content locally by scraper

--- Projects/scraper/home/script.py
+++ Projects/scraper/home/script.py
@@ -1,6 +1,26 @@
 import requests
 from bs4 import BeautifulSoup
 from .models import News
+import os
+import uuid
+
+# Downloading images locally
+def download_image(image_url, save_directory, image_name):
+    if os.path.exists(save_directory):
+        os.makedirs(save_directory)
+    image_path = os.path.join(save_directory, image_name)
+
+    # To download image use stream = True
+    response = requests.get(image_url, stream=True)
+
+    if response.status_code == 200:
+        with open(image_name, 'wb') as file:
+            # 1024 is chunk size
+            for chunk in response.iter_content(1024):
+                file.write(chunk)
+
+    return image_url
+
 
 def scrap_news():
     url = "https://www.imdb.com/news/movie/"
@@ -26,6 +46,12 @@ def scrap_news():
         title = title.text.strip() if title else "No Title"
         desc = desc.text.strip() if title else "No desc"
         image = image['src']
+
+        image_path = None
+
+        if image:
+            image_name = f"image_{uuid.uuid4()}.jpg"
+            image_path = download_image(image, 'downloads', image_name)
         
         news = {
             "title": title,

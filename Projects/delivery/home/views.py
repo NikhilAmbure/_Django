79109ff52b2fcd31f6951272cd3677780456c305
Nikhil Ambure <nikhilambure25@gmail.com>
2025-06-21 17:41:44 +0530

    'Real_time_Delivery_Locations'

--- Projects/delivery/home/views.py
+++ Projects/delivery/home/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from .models import *
+from django.http import JsonResponse
 
 # Create your views here.
+
+def index(request):
+    return render(request, 'index.html')    
+
+
+def get_data(request):
+    latest_data = LocationUpdate.objects.latest('timestpamp')
+    return JsonResponse({
+        'latitude': latest_data.latitude,
+        'longitude': latest_data.longitude, 
+        'timestpamp': latest_data.timestpamp
+    })

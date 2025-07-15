d64046c06e37914bb21f2028dc08fb2a1e7a5161
Nikhil Ambure <nikhilambure25@gmail.com>
2025-07-15 21:53:51 +0530

    'Docker;

---
+++ Projects/core/home/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.http import JsonResponse
+
+# Create your views here.
+
+def index(request):
+    data = {
+        'status': 'success',
+        'message': 'Hello World',
+    }
+    return JsonResponse(data)
